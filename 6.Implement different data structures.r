da2a2548c692b0c83bf36059b6526fac3dc7c33a
11249a187-creator <11249a187@kanchiuniv.ac.in>
2026-04-22 23:31:54 +0530

    Create 6.Implement different data structures.r

---
+++ 6.Implement different data structures.r
@@ -0,0 +1,137 @@
+6.1
+Student Marks Processing (Vectors)
+R program:
+marks <- c(88, 58, 95, 98, 67)
+total <- sum(marks)
+average <- mean(marks)
+highest <- max(marks)
+total
+average
+highest
+
+6.2:
+Temperature Monitoring System (Vectors)
+R program:
+temperature <- c(70, 83, 95, 67, 90, 98, 59)
+min_temp <- min(temperature)
+max_temp <- max(temperature)
+min_temp
+
+max_temp
+
+6.3:
+Employee Profile Storage (Lists)
+R program:
+List is suitable because it supports different data types.
+employee <- list(
+ID = 107,
+Name = "Badri",
+Salary = 150000,
+Department = "CSE"
+)
+employee
+employee$Name
+employee$Salary
+
+6.4:
+Patient Medical Record System (Lists)
+R program:
+patient <- list(
+PatientID = 203,
+Name = "Badri",
+Age = 23,
+TestResults = c(110, 140, 130)
+)
+patient
+patient$TestResults
+
+Question
+6.5:
+College Student Database (Data Frames)
+R program:
+Data frame is best for structured tabular data.
+students <- data.frame(
+
+RollNo = c(1, 2, 3),
+Name = c("Badri", "Dhatri", "srinu),
+Dept = c("CSE", "ECE", "IT"),
+Marks = c(88, 78, 92)
+)
+students
+Question
+6.6:
+Sales Report System (Data Frames)
+R program:
+sales <- data.frame(
+Month = c("Jan", "Jul", "Sep"),
+Sales = c(50000, 62000, 58000)
+)
+total_sales <- sum(sales$Sales)
+total_sales
+
+Question6.7:
+E-Commerce Order System (Combination of Data Structures)
+R program:
+prices <- c(1100, 850, 560)
+customer <- list(
+CustomerID = 301,
+Name = "Badri",
+City = "Kadapa"
+)
+orders <- data.frame(
+Item = c("Phone", "Headset", "Charger"),
+Price = prices
+)
+prices
+customer
+orders
+
+6.8:
+Online Exam System
+R program:
+subject_marks <- c(78, 85, 88)
+student_profile <- list(
+Name = "Badri",
+RollNo = 105,
+Marks = subject_marks
+)
+class_results <- data.frame(
+RollNo = c(101, 102, 103),
+Total = c(240, 255, 270)
+)
+student_profile
+class_results
+
+6.9:
+Bank Account Management
+R program
+balances <- c(5000, 5200, 5100)
+customer <- list(
+AccountNo = 12345,
+Name = "Badri"
+)
+transactions <- data.frame(
+Date = c("01-01", "02-01", "03-01"),
+Amount = c(-500, 1500, -200)
+)
+balances
+customer
+transactions
+
+6.10:
+Real-World Summary Question (Exam-Perfect)
+R program:
+Vector stores homogeneous data (marks, prices)
+List stores heterogeneous data (profiles)
+Data Frame stores structured tabular data (records)
+scores <- c(85, 90, 88)
+profile <- list(
+Name = "Badri",
+Age = 21,
+Scores = scores
+
+)
+records <- data.frame(
+Subject = c("Math", "CSA", "Physics"),
+Marks = scores
+)
